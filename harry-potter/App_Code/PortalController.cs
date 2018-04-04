fbb325dd8e3ceec706ad0854ad539cbb73c921a5
Syntasu <abouma18@hotmail.com>
2018-04-04 14:45:37 +0200

    Hooked up portal page stats with database.

---
+++ harry-potter/App_Code/PortalController.cs
@@ -0,0 +1,79 @@
+﻿using WebMatrix.Data;
+
+public class PortalController
+{
+    public int GetHouseMemberCount(int houseId)
+    {
+        Database db = Database.Open("Database");
+        string query = "SELECT uHouse FROM users WHERE uHouse=@0";
+        dynamic result = db.Query(query, houseId);
+
+        return result.Count;
+    }
+
+    public int GetHousePoints(int houseId)
+    {
+        Database db = Database.Open("Database");
+        string query = "SELECT hPoints FROM houses WHERE hId=@0";
+        dynamic result = db.Query(query, houseId);
+        
+        if(result.Count > 0)
+        {
+            return result[0].hPoints;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    public int GetHouseRank(int houseId)
+    {
+        Database db = Database.Open("Database");
+        string query = "SELECT COUNT(*) AS rank FROM houses WHERE hPoints >= (SELECT hPoints FROM houses WHERE hId=@0)";
+
+        dynamic result = db.Query(query, houseId);
+
+        if (result.Count > 0)
+        {
+            return result[0].rank;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    public int GetUserPoints(string username)
+    {
+        Database db = Database.Open("Database");
+        string query = "SELECT uPoints FROM users WHERE uUname=@0";
+        dynamic result = db.Query(query, username);
+
+        if(result.Count > 0)
+        {
+            return result[0].uPoints;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    public int GetUserRank(string username)
+    {
+        Database db = Database.Open("Database");
+        string query = "SELECT COUNT(*) AS rank FROM users WHERE uPoints >= (SELECT uPoints FROM users WHERE uUname=@0)";
+
+        dynamic result = db.Query(query, username);
+
+        if(result.Count > 0)
+        {
+            return result[0].rank;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
